d7ae9d4d1ab3338f0b2bdb2dcf5d8f95d085bfed
= <=>
2024-09-08 18:07:09 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,8 +1,12 @@
-let bill = 250.00;
-let tip = bill <= 300 ? bill*0.15 : bill*0.2;
+let bill = 25.00;
+let tip = bill <= 300 && bill >= 50 ? bill*0.15 : bill*0.2;
 let total = bill+tip;
 
 console.log(`Your bill was: $${bill}`);
 console.log(`Your tip amount is: $${tip}`);  
 console.log(`This brings your total to: $${total}`); 
 
+function calculateTip (a) {
+return a <= 300 && a >= 50 ? a*0.15 : a*0.20 }
+
+console.log (calculateTip (100))
